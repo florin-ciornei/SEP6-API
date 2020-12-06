5ef08c373d59301c5c397a015ee5c68fdf9a6a28
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-12-06 09:24:45 +0100

    Small console.log change

--- index.js
+++ index.js
@@ -36,5 +36,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`REST API listening at http://localhost:${port}`)
 })
