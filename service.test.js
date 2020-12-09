085874224be0e099557a9bcdac897d4139eea6cd
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-12-09 14:50:36 +0100

    Add more tests

--- service.test.js
+++ service.test.js
@@ -1,6 +1,36 @@
 const service = require("./service");
 
-test('to have 1459 origins', async () => {
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://sepMongo:mongo@cluster0.besa8.mongodb.net/sep6?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => {
+        console.log("Connected to MongoDB via Mongoose");
+    });
+
+
+test('Origins returns 3 origins', async () => {
     let origins = await service.getOrigins();
-    expect(origins.length).toBe(1458);
+    expect(origins.length).toBe(3);
+});
+
+test('Origins has a single JFK', async () => {
+    let origins = await service.getOrigins();
+    expect(origins.filter((origin) => origin.faa === "JFK").length).toBe(1);
+});
+
+test('Origins has a single EWR', async () => {
+    let origins = await service.getOrigins();
+    expect(origins.filter((origin) => origin.faa === "EWR").length).toBe(1);
+});
+
+test('Origins has a single LGA', async () => {
+    let origins = await service.getOrigins();
+    expect(origins.filter((origin) => origin.faa === "LGA").length).toBe(1);
+});
+
+afterAll(async () => {
+    return await mongoose.disconnect();
 });
