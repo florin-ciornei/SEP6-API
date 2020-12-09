5e2752e82779fe65f33a1b5460f88b43ed89c931
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-12-09 15:30:53 +0100

    Generate example documentation

---
+++ doc/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "SEP6",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-12-09T14:26:24.224Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
