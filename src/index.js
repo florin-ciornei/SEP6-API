40f783f25db9d21fb70ceaa0eb16b6f4215c6cc4
florin-ciornei <31960645+florin-ciornei@users.noreply.github.com>
2020-12-16 15:30:10 +0100

    Test CI/CD

--- src/index.js
+++ src/index.js
@@ -306,7 +306,7 @@ app.get('/noOfFlightsPerManufacter', async (req, res) => {
  */
 app.get('/', (req, res) => {
     res.send("Welcome to our SEP6 project !!! A RESTful API built by " +
-        "Andrei, Daniela and Florin. ");
+        "Andrei, Daniela and Florin. Test CI/CD");
 });
 
 app.listen(port, () => {
